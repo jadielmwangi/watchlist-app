39b3134125d45c3d949b80b95ed4d983f7ea2fda
jadielmwangi <jedielmwangi@gmail.com>
2020-04-22 14:04:54 +0300

    to group blocks of our code's logic that will be reused multiple times

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
+
 from flask import Flask
 from .config import DevConfig
+# from app import views
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -8,4 +10,3 @@ app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
-from app import views
--- app/request.py
+++ app/request.py
@@ -1,4 +1,64 @@
+# from app import app
+
+# # Getting api key
+# api_key = app.config['4b5875e5fc8e9e3c5c43dc4be409b591']
+
 from app import app
+import urllib.request,json
+from .models import movie
+
+Movie = movie.Movie
 
 # Getting api key
-api_key = app.config['MOVIE_API_KEY']
+api_key = app.config['4b5875e5fc8e9e3c5c43dc4be409b591']
+
+# Getting the movie base url
+base_url = app.config["https://api.themoviedb.org/3/movie/550?api_key=4b5875e5fc8e9e3c5c43dc4be409b591"]
+
+
+def get_movies(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_movies_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results = None
+
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+            movie_results = process_results(movie_results_list)
+
+
+    return movie_results
+
+
+    ## Processing Results
+
+def process_results(movie_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    movie_results = []
+    for movie_item in movie_list:
+        id = movie_item.get('id')
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+
+        if poster:
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            movie_results.append(movie_object)
+
+    return movie_results
